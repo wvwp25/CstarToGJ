2fdb83ff54d54f8c6e5aa1c0cb5d01ecac6a1131
Hsiao Chen <hsiaoche@cern.ch>
2026-02-03 04:09:43 +0100

    fit range [700,1300]

--- Data/build_bkg_workspace.C
+++ Data/build_bkg_workspace.C
@@ -3,7 +3,10 @@ void build_bkg_workspace() {
     TFile f("Run2017BCDEF_BG_ana.root");
     TF1* f_bkg = (TF1*)f.Get("bkgFit");
 
-    RooRealVar x("x", "m_{#gamma j}", 700, 3000);
+    double M = 1000.0;
+    double xmin = M - 300.0;
+    double xmax = M + 300.0;
+    RooRealVar x("x", "m_{#gamma j}", xmin, xmax);
 
     RooRealVar P0("P0","P0", f_bkg->GetParameter(0));
     RooRealVar P1("P1","P1", f_bkg->GetParameter(1));
@@ -26,29 +29,19 @@ void build_bkg_workspace() {
     RooDataHist data_obs("data_obs", "data_obs", x, hData);
 
     // Find the bin numbers corresponding to the range [700, 3000]
-    double M = 1000.0;
-    double xmin_fit = M - 300.0;
-    double xmax_fit = M + 300.0;
-    int bin_start = hData->GetXaxis()->FindBin(700.0);
-    int bin_end   = hData->GetXaxis()->FindBin(3000.0);
-    int bin_low = hData->GetXaxis()->FindBin(xmin_fit);
-    int bin_high   = hData->GetXaxis()->FindBin(xmax_fit);
+    int bin_start = hData->GetXaxis()->FindBin(xmin);
+    int bin_end   = hData->GetXaxis()->FindBin(xmax);
 
     // Integrate only within those bins
     double Nbkg = hData->Integral(bin_start, bin_end);
-    std::cout << "Background Yield in [700, 3000]: " << Nbkg << std::endl;
-
-    double Nbkg_fit = hData->Integral(bin_low, bin_high);
-    std::cout << "Background Yield in [" << xmin_fit << "," << xmax_fit << "]: " << Nbkg_fit << std::endl;
+    std::cout << "Background Yield in [" << xmin << "," << xmax << "]: " << Nbkg << std::endl;
 
     RooRealVar nbkg("nbkg", "background yield", Nbkg, 0, 1e6);
-    RooRealVar nbkg_fit("nbkg_fit", "background yield (fit)", Nbkg_fit, 0, 1e6);
     RooExtendPdf bkg_ext("bkg_ext", "extended background", bkg, nbkg);
 
 
     RooWorkspace ws("ws","background workspace");
     ws.import(x);
-    ws.import(nbkg_fit);
     ws.import(bkg_ext);
     ws.import(data_obs);
 
